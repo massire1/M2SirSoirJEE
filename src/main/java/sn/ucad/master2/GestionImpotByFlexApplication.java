164f94baaa9ae402fdcc8984d23917012f29397f
Masta <Masta@MASTA-PC>
2020-04-13 13:04:09 +0000

    Commentaire de changement du root context Path

--- src/main/java/sn/ucad/master2/GestionImpotByFlexApplication.java
+++ src/main/java/sn/ucad/master2/GestionImpotByFlexApplication.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import sn.ucad.master2.repository.ICatalogueDaoRepository;
 import sn.ucad.master2.service.HabitationService;
 
 @SpringBootApplication
-public class GestionImpotByFlexApplication extends SpringBootServletInitializer implements CommandLineRunner {
+public class GestionImpotByFlexApplication implements CommandLineRunner {
 
 	@Autowired
 	ICatalogueDaoRepository myCatalogueRepository;
